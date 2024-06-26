a174e1fbc3028744bbbad4a133b689bf647bdfe0
Naushad101 <naushad.shaikh@bnt-soft.com>
2024-06-26 13:04:17 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.logging.Logger;
+
+public class Main {
+
+   private final static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+        logger.info("Pogramm started");
+
+        BufferedWriter writer=null;
+        try{
+         String inputFile = "C:\\Users\\naushad.shaikh\\Desktop\\input.txt";
+            String outPutFile = "C:\\Users\\naushad.shaikh\\Desktop\\output.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            writer = new BufferedWriter(new FileWriter(outPutFile));
+
+            String line=null;
+            while((line=reader.readLine())!=null){
+                String res = reverse(line);
+                System.out.println(res);
+                writer.write(res);
+                writer.newLine();
+
+            }
+
+
+        }
+        catch (Exception e){
+            logger.warning("File Not Found Exception accured"+e.getMessage());
+        }
+        finally {
+            try {
+                if(writer!=null){
+                    writer.close();
+             }
+            }
+            catch (Exception e){
+                logger.info("Error closing file");
+            }
+        }
+    }
+
+    public static String reverse(String s){
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i>=0; i--){
+            sb.append(reverseWord(arr[i])+" ");
+        }
+        return sb.toString().trim();
+    }
+    public static String reverseWord(String s){
+        StringBuilder sb = new StringBuilder();
+        char ch = ' ';
+        for(int i = s.length()-1; i>=0; i--){
+            if(s.charAt(i)=='.' || s.charAt(i)==',' || s.charAt(i)=='!'){
+                ch=s.charAt(i);
+                continue;
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
+        sb.append(ch);
+        return sb.toString();
+    }
+}
